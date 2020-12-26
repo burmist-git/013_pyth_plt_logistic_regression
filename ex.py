9d97e17b4e66acaaa98916b67d9a41722b9aad03
burmist <burm.leonid@gmail.com>
2020-12-26 20:40:16 +0100

    update

--- ex.py
+++ ex.py
@@ -10,8 +10,9 @@ Description : Simple reminder-training example.
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from optparse import OptionParser
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
+#from sklearn.metrics import classification_report, confusion_matrix
 
 np.random.seed(1234567890)
 
@@ -28,8 +29,27 @@ def getHistBins(nBins,xMin,xMax):
     binW = (xMax - xMin)/nBins
     return [xMin + binW*i for i in range(nBins+1)]
 
-def plot(datax,datay,bins):
+def sigmoidFunction(k,x):
+    pol=polynomialFunction(k,x)
+    return 1/(1+np.exp(-pol))
 
+def polynomialFunction(k,x):
+    return np.dot(k,x)
+
+def printArrayInfo(ar,ar_name,printkey=False):
+    print('')
+    print(ar_name)
+    print('{}.ndim  = '.format(ar_name), ar.ndim)
+    print('{}.shape = '.format(ar_name), ar.shape)
+    print('type({}) = '.format(ar_name), type(ar))
+    if printkey :
+        print('{} = '.format(ar_name), ar)
+
+def plot(nn,datax,datay,bins,weights,binsCenter,y,y_s):
+
+    #printArrayInfo(y,'y')
+    #printArrayInfo(y_s,'y_s')
+    
     nPlots=len(datax) + 2
     ncols=math.ceil(nPlots**(0.5))
     nrows=int(nPlots/ncols)+math.ceil(nPlots/ncols-int(nPlots/ncols))
@@ -50,70 +70,116 @@ def plot(datax,datay,bins):
             if(plotID<len(datax)):
                 axs[i,j].hist(datax[plotID], bins=bins)
             elif (plotID == len(nn)):
-                axs[i,j].hist(np.vstack(datax), bins=bins, weights=(1.0/700*np.ones(np.vstack(datax).shape)))
+                axs[i,j].hist(np.vstack(datax), bins=bins, weights=(weights*np.ones(np.vstack(datax).shape)),alpha=0.5)
+                #axs[i,j].scatter(binsCenter,y)
+                if type(binsCenter) != 'NoneType':
+                    axs[i,j].scatter(binsCenter,y_s,facecolor='blue')
             elif (plotID == (len(nn)+1)):
                 axs[i,j].scatter(np.vstack(datax),np.hstack(datay))
     
     plt.show()
 
-
-if __name__ == "__main__":
-
+def pol01():
     classType = [0,1]
-    nn    = [10000,10000]
-    x0    = [1,2]
-    sigma = [0.3,0.3]
+    nn = [10000,10000]
+    x0 = [-6-10,3]
+    sigma = [0.6,3.0]
     nPoints = 4
     nBins = 100
     nSigma = 4
     
     datax, datay = generateData(nn,x0,sigma,classType)
-    print('')
-    print('datax')
-    print('datax.ndim  = ', datax.ndim)
-    print('datax.shape = ', datax.shape)
-    print('type(datax) = ', type(datax))
-    print('datax = ', datax)
-    print('')
-    print('datay')
-    print('datay.ndim  = ', datay.ndim)
-    print('datay.shape = ', datay.shape)
-    print('type(datay) = ', type(datay))
-    print('datay = ', datay)
+    printArrayInfo(datax,'datax',printkey=False)
+    printArrayInfo(datay,'datay',printkey=False)
     
     xMin = int(np.array(x0).min() - math.ceil(nSigma*np.array(sigma).max()))
     xMax = int(np.array(x0).max() + math.ceil(nSigma*np.array(sigma).max()))
     bins = getHistBins(nBins,xMin,xMax)
+    binsCenter = (bins[1]-bins[0])/2.0 + np.array(bins)
+    binsCenter = binsCenter[0:(len(binsCenter)-1)]    
     #print(len(bins))
 
     model = LogisticRegression(solver='liblinear', random_state=0)
     model.fit(datax.reshape((-1,1)), datay.reshape((-1,)))
 
+    print('')
     print('model.classes_   = ',model.classes_)
     print('model.intercept_ = ',model.intercept_) 
     print('model.coef_      = ',model.coef_)
 
+    k = np.hstack(np.array([np.squeeze(model.intercept_),np.squeeze(model.coef_)]))
+    printArrayInfo(k,'k',printkey=True)
 
-    plot(datax,datay,bins)
+    y = polynomialFunction(k, (np.row_stack((np.ones(len(binsCenter)),binsCenter))))
+    y_s = sigmoidFunction(k, (np.row_stack((np.ones(len(binsCenter)),binsCenter))))
     
+    plot(nn,datax,datay,bins,1.0/700,binsCenter,y,y_s)
+    #plot(datax,datay,bins,1.0/700,binsCenter=None,y=None,y_s=None)
 
-    '''
-    #np.column_stack((D_model, np.ones(D.shape[0])))
-    x = np.ones(nPoints)
-    x = np.column_stack((x,np.linspace(xMin, xMax, nPoints)))
-    #x = np.column_stack((x,np.linspace(xMin, xMax, nPoints)))
-    #x = np.expand_dims(np.linspace(xMin, xMax, nPoints),axis=1)
-    #k = np.expand_dims(np.array([1,2,0.3]),axis=1)
-    #print('theta.ndim  = ', k.ndim)
-    #print('theta.shape = ', k.shape)
-    print('x.ndim      = ', x.ndim)
-    print('x.shape     = ', x.shape)
-    print('x           = ', x)
-    '''
-    #x = np.arange(10)
-    #x = np.stack((x,np.arange(10)))
-    #x = x.reshape(-1,1,2)
-    #x = np.squeeze(x)
-    #print('x.ndim      = ', x.ndim)
-    #print('x.shape     = ', x.shape)
-    #print('x           = ', x)
+def pol02():
+    classType = [0,1,0]
+    nn = [10000,10000,10000]
+    x0 = [-6-10,3,6+10]
+    sigma = [0.6,0.1,0.6]
+    nPoints = 4
+    nBins = 100
+    nSigma = 4
+    
+    datax, datay = generateData(nn,x0,sigma,classType)
+    printArrayInfo(datax,'datax',printkey=False)
+    printArrayInfo(datay,'datay',printkey=False)
+
+    printArrayInfo(datax.reshape((-1,1)),'datax.reshape((-1,1))',printkey=False)
+    datax_r=np.column_stack([datax.reshape((-1,1)),datax.reshape((-1,1))*datax.reshape((-1,1))])
+    printArrayInfo(datax_r,'datax_r',printkey=False)
+    
+    xMin = int(np.array(x0).min() - math.ceil(nSigma*np.array(sigma).max()))
+    xMax = int(np.array(x0).max() + math.ceil(nSigma*np.array(sigma).max()))
+    bins = getHistBins(nBins,xMin,xMax)
+    binsCenter = (bins[1]-bins[0])/2.0 + np.array(bins)
+    binsCenter = binsCenter[0:(len(binsCenter)-1)]    
+    #print(len(bins))
+
+    model = LogisticRegression(solver='liblinear', random_state=0)
+    model.fit(datax_r, datay.reshape((-1,)))
+
+    print('')
+    print('model.classes_   = ',model.classes_)
+    print('model.intercept_ = ',model.intercept_) 
+    print('model.coef_      = ',model.coef_)
+
+    k = np.hstack([np.squeeze(model.intercept_),np.squeeze(model.coef_)])
+    printArrayInfo(k,'k',printkey=True)
+    #printArrayInfo(np.row_stack((np.ones(len(binsCenter)),binsCenter)),'a',printkey=False)
+    #printArrayInfo(np.row_stack((np.row_stack((np.ones(len(binsCenter)),binsCenter)), binsCenter*binsCenter)),'b',printkey=False)
+    y = polynomialFunction(k, np.row_stack((np.row_stack((np.ones(len(binsCenter)),binsCenter)), binsCenter*binsCenter)))
+    y_s = sigmoidFunction(k, np.row_stack((np.row_stack((np.ones(len(binsCenter)),binsCenter)), binsCenter*binsCenter)))
+    
+    plot(nn,datax,datay,bins,1.0/700,binsCenter,y,y_s)
+    #plot(datax,datay,bins,1.0/700,binsCenter=None,y=None,y_s=None)
+
+parser = OptionParser()
+parser.add_option('-p', '--pol',
+                  dest='poldeg', type="int",default=1,
+                  help="polinom degree")
+
+(options, args) = parser.parse_args()
+    
+if __name__ == "__main__":
+    if options.poldeg == 1:
+        pol01()
+    elif options.poldeg == 2:
+        pol02()
+    else :
+        print(options)
+        print(type(options))
+        print(args)
+        print(type(args))
+        print('len(args) = ',len(args))
+        print('type(options.poldeg)        = ',type(options.poldeg))
+        print('parser.get_usage()          = ',parser.get_usage())
+        print('parser.get_default_values() = ',parser.get_default_values())
+        print('parser.get_prog_name()      = ',parser.get_prog_name())
+        #print(parser.get_version())
+        #print(parser.get_description())
+        #print(parser.get_option_group())
